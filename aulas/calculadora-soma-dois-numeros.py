4c0e0eb21cd553d33d1e83a228e1ed65ecfcfe6f
Bianca <bianca.trotta@hotmail.com>
2024-06-08 23:54:53 -0300

    feat: 08/06

---
+++ aulas/calculadora-soma-dois-numeros.py
@@ -0,0 +1,14 @@
+#algoritmo calculadora
+#objetivo: calcular a soma de 2 numeros
+#entrada: 2 numeros
+#saida: soma dos numeros
+#declaraçoes: 
+#inteiro: numero1, numero2, soma
+
+print("Escreva um número")
+numero1 = input()
+print("Escreva outro número")
+numero2 = input()
+
+print("A resposta é", int(numero1) + int(numero2))
+
---
+++ aulas/celsius-into-farenheit.py
@@ -0,0 +1,15 @@
+#Entrada: escreva("digite a temperatura em Celsius")
+#leia(C)
+#Saída: escreva("a temperatura é", Farenheit)
+#Declarações: real celsius farenheit
+
+#INSTRUÇOES:
+
+print("Digite e a temperatura em Celsius")
+C = input()
+
+#PROCESSAMENTO:
+
+F = ((int(C)*9) / 5) + 32
+
+print("O valor em Farenheit é", F)
